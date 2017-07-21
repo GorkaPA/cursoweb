2b16587ec2e6c871cd4198f8a1cd2638cc4a7039
GorkaPA <gorkitap@hotmail.com>
2017-07-21 13:15:19 +0200

    vv

--- TuLibroEnCasa/WebContent/js/registro.js
+++ TuLibroEnCasa/WebContent/js/registro.js
@@ -1,6 +1,12 @@
 /**
  * 
  */
+
+window.onload = function disableBtn() {
+    document.getElementById("myBtn").disabled = true;
+}
+
+
 function validatePassword() {
 	var clave1 = document.getElementById("clave").value;
 	var clave2 = document.getElementById("confClave").value;
