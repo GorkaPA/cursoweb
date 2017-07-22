bef2be36ae922e949962b0eb6c4364a733137734
GorkaPA <gorkitap@hotmail.com>
2017-07-22 19:41:15 +0200

    vv

--- TuLibroEnCasa/src/tulibroencasa/servlets/SrvRegistro.java
+++ TuLibroEnCasa/src/tulibroencasa/servlets/SrvRegistro.java
@@ -1,39 +1,22 @@
 package tulibroencasa.servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 /**
  * Servlet implementation class SrvRegistro
  */
 public class SrvRegistro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SrvRegistro() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		ServletContext ct = getServletContext();
+		RequestDispatcher rd = ct.getRequestDispatcher("/registro.jsp");
+		rd.forward(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
