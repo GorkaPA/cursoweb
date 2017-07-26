fd55e3924994a849f783b8035a2df14785fb7e4c
GorkaPA <gorkitap@hotmail.com>
2017-07-26 10:54:16 +0200

    buscarlibros

--- TuLibroEnCasa/src/tulibroencasa/servlets/SrvBuscarLibros.java
+++ TuLibroEnCasa/src/tulibroencasa/servlets/SrvBuscarLibros.java
@@ -8,9 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import tulibroencasa.bd.BdOperaciones;
 
 /**
  * Servlet implementation class SrvBuscarLibros
