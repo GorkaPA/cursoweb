fdd6cd16736700680fc23b7314e917a8fbe73cb4
GorkaPA <gorkitap@hotmail.com>
2017-07-26 10:20:03 +0200

    buscalibros

--- TuLibroEnCasa/src/tulibroencasa/servlets/SrvBuscarLibros.java
+++ TuLibroEnCasa/src/tulibroencasa/servlets/SrvBuscarLibros.java
@@ -1,39 +1,26 @@
 package tulibroencasa.servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import tulibroencasa.bd.BdOperaciones;
 
 /**
  * Servlet implementation class SrvBuscarLibros
  */
 public class SrvBuscarLibros extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SrvBuscarLibros() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		ServletContext ct = getServletContext();
+		RequestDispatcher rd = ct.getRequestDispatcher("/buscarLibros.jsp");
+		rd.forward(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
